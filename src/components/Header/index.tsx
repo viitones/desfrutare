21c92aad884e63195bdfe68f948c5ea9f6b83bd6
viitones <vitonesmartins@gmail.com>
2026-04-14 18:39:29 -0300

    :sparkles: feat: add botao inscricao

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,45 +1,70 @@
-import './index.css'
-import logo from '../../assets/logo.png'
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import "./index.css";
 
-import hamb from '../../assets/icon.svg'
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
+import hamb from "../../assets/icon.svg";
 
 export function Header() {
-  const [close, setClose] = useState(false)
+  const [close, setClose] = useState(false);
 
   function handleClickOutside(event: MouseEvent) {
-    const nav = document.getElementById('mynav')
+    const nav = document.getElementById("mynav");
     if (nav && !nav.contains(event.target as Node)) {
-      setClose(false)      
+      setClose(false);
     }
   }
 
   useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside)
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
-    }
-  }, [handleClickOutside])
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [handleClickOutside]);
 
   function handleCloseNav() {
-    setClose(true)
+    setClose(true);
   }
   function handleOpenNav() {
-    setClose(false)
+    setClose(false);
   }
 
-    return (
+  return (
     <header>
       <div>
         <Link to="/" className="img-container">
           <img src={logo} alt="infinity institute" />
         </Link>
         <nav>
-          <Link className='link' to="/">Início</Link>
-          <a className='link' href="/#modalidade">Modalidades</a>
-          <Link className='link' to="/transparence">Transparência</Link>
-          <a className='link' href="/#contato">Contato</a>
+          <Link className="link" to="/">
+            Início
+          </Link>
+          <a className="link" href="/#modalidade">
+            Modalidades
+          </a>
+          <Link className="link" to="/transparence">
+            Transparência
+          </Link>
+          <a className="link" href="/#contato">
+            Contato
+          </a>
+        </nav>
+
+        <nav className="flex gap-3">
+          <Link
+            target="_blank"
+            className="btn-nav"
+            to="https://docs.google.com/forms/d/e/1FAIpQLSctUvaSeFAknAe-gx8_W1xZcCTsGvDzVcUwB_e2pi2jjmo8eg/closedform"
+          >
+            Inscrição Vôlei
+          </Link>
+          <Link
+            target="_blank"
+            className="btn-nav"
+            to="https://docs.google.com/forms/d/e/1FAIpQLSfDXFpsZ7ptKdKW4LWIWpOSLuwjOITpqF9pEFhiy8VMtG20Fg/viewform"
+          >
+            Inscrição Judô
+          </Link>
         </nav>
 
         <div id="mynav" className="overlay" data-close={close}>
@@ -49,9 +74,13 @@ export function Header() {
 
           <div className="conteudo">
             <Link to="/">Início</Link>
-            <a className="cls" href="/#modalidade">Modalidades</a>
+            <a className="cls" href="/#modalidade">
+              Modalidades
+            </a>
             <Link to="/transparence">Transparência</Link>
-            <a className="cls" href="/#contato">Contato</a>
+            <a className="cls" href="/#contato">
+              Contato
+            </a>
           </div>
         </div>
 
@@ -60,5 +89,5 @@ export function Header() {
         </button>
       </div>
     </header>
-  )
+  );
 }
